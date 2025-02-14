bee43ce55894810a7df3fbb649a6bd4ee6812d35
JeongHunHui <deca3248@gmail.com>
2025-02-14 20:10:34 +0900

    chore#2: root의 gradle 설정

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.4.1" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+allprojects {
+    group = "com.techeer"
+    version = "1.0.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    // 공통 의존성
+    dependencies {
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.25")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.3")
+        testImplementation("io.mockk:mockk:1.13.16")
+    }
+}
