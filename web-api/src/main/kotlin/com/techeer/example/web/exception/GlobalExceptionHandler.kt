16dbbfb299372944ebf705a492a1b84ed240b4ae
JeongHunHui <deca3248@gmail.com>
2025-02-14 20:49:30 +0900

    chore#3: 발생하는 예외를 handling하여 logging 및 공통 예외 응답으로 파싱하여 반환하는 GlobalExceptionHandler 설정

---
+++ web-api/src/main/kotlin/com/techeer/example/web/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,30 @@
+package com.techeer.example.web.exception
+
+import com.techeer.example.common.error.BusinessException
+import com.techeer.example.common.error.GlobalErrorCode
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@RestControllerAdvice
+class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
+    @ExceptionHandler(Exception::class)
+    protected fun handleException(e: Exception): ResponseEntity<ErrorResponse.Body> {
+        logger.error(e.message, e)
+        return ErrorResponse(
+            errorCode = GlobalErrorCode.INTERNAL_SERVER_ERROR,
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+        )
+    }
+
+    @ExceptionHandler(BusinessException::class)
+    protected fun handleRuntimeException(e: BusinessException): ResponseEntity<ErrorResponse.Body> {
+        logger.warn(e.toString())
+        return ErrorResponse(
+            errorCode = e.errorCode,
+            httpStatus = HttpStatus.BAD_REQUEST,
+        )
+    }
+}
