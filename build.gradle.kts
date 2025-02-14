f6e2104163e6138894a558ea8c8344376d41f79f
JeongHunHui <deca3248@gmail.com>
2025-02-14 21:14:25 +0900

    chore#7: ktlint 의존성 및 editorconfig 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     kotlin("plugin.jpa") version "1.9.25" apply false
     id("org.springframework.boot") version "3.4.1" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
+
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
 }
 
 allprojects {
@@ -17,6 +19,7 @@ allprojects {
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     kotlin {
         compilerOptions {
